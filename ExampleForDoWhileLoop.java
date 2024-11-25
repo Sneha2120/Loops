a978e867d9228925a40cc48f0644ce887dd9eb38
Sneha2120 <snehagowdas473@gmail.com>
2024-11-25 14:16:01 +0530

    Add files via upload

---
+++ ExampleForDoWhileLoop.java
@@ -0,0 +1,18 @@
+package com.anpc8323.day3;
+
+public class ExampleForDoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		
+		do
+		{
+			System.out.println(x);
+			x--;
+		}
+		while(x>=1);
+
+	}
+
+}
